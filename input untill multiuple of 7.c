0b634c602de8a910d2edd9820a316c8ec170dc85
mdjaidjamil0556-dot <mdjaidjamil0556@gmail.com>
2026-06-17 15:07:16 +0530

    Add files via upload

---
+++ input untill multiuple of 7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () {
+    int n; // input untill multiple of 7
+    d0 {
+        printf("enter a number : ");
+        scanf("%d", &n);
+        printf("%d\n", n);
+        if (n % 7==0){
+            break;
+        }
+    } while(1);
+    printf("THANK YOU");
+    return 0;
+}
