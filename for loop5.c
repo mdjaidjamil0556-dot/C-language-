afc98ffbc50f4259fe13c377cfae8fc95e4d3b52
mdjaidjamil0556-dot <mdjaidjamil0556@gmail.com>
2026-08-03 17:21:00 +0530

    Add files via upload

---
+++ for loop5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n, i;
+    long long fact = 1;
+    printf("enter a number");
+    sacnf("%d", &n);
+    for(i = 1; i<=n; i++){
+        fact *=i;
+    }
+    printf("factorial = %lld", fact);
+    return 0;
+}
